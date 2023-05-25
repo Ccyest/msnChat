75685b3df20de6a148294458c8c442300755f652
Yichi Zhang <109014949+Ccyest@users.noreply.github.com>
2023-05-26 00:13:53 +0800

    set up the project

---
+++ msnChat/chat/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+SEX_CHOICES = (
+            ('Male', 'Male'),
+            ('Female', 'Female'),
+            ('Other', 'Other'),
+        )
+
+# Create your models here.
+
+class User (models.Model):
+    email = models.EmailField(unique=True, primary_key=True)
+    avatar = models.ImageField(upload_to="msn/image/avatar/",blank=True)
+    gender = models.CharField(max_length=20, choices = SEX_CHOICES, blank=True)
+    bio = models.TextField(max_length=3000, blank=True)
+
+class Tag (models.Model):
+    user = models.ForeignKey(User, related_name='tags', on_delete=models.CASCADE)
+    content = models.CharField(max_length=20, primary_key=True)
+
+
+# --------- function ------------ #
