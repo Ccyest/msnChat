ee779bcb23c9923ada35f0be2f050ba5377d7dfb
Yichi Zhang <109014949+Ccyest@users.noreply.github.com>
2023-05-27 15:30:21 +0800

    set up React and added test it working

--- msnChat/msnChat/settings.py
+++ msnChat/msnChat/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "chatAPI.apps.ChatConfig",
-    "rest_framework"
+    "rest_framework",
+    "frontend.apps.FrontendConfig"
 ]
 
 MIDDLEWARE = [
--- msnChat/msnChat/urls.py
+++ msnChat/msnChat/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('chat/',include('chatAPI.urls'))
+    path('chatAPI/',include('chatAPI.urls')),
+    path('',include('frontend.urls'))
 ]
